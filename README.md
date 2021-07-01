f9b5504ec2be3a064f517477f4dc0490ba080d00
Daniel Sobral <dcsobral@gmail.com>
2021-07-01 15:08:49 -0600

    Move items to the inventory in preference to the hotbar

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# raft-inventory-before-toolbelt
-Raft Mod changing the preference order between inventory and toolbelt
+# Raft Mod
+
+In normal Raft when items are moved into a player's inventory, whether by harvesting, looting or
+shift clicking, they go first to the hotbar and only if the hotbar is full they'll go into the
+inventory. This mod inverts the preference between these two so that items will only be moved into
+the hotbar if there are no more empty slots in the inventory. Note that this does not affect at
+all the ability to move items between inventory and hotbar with a shift click.
---
+++ inventory-before-hotbar/inventory-before-hotbar.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using HarmonyLib;
+using System.Reflection;
+
+public class InventoryBeforeHotbar : Mod
+{
+    public Harmony harmony;
+
+    public void Start()
+    {
+        harmony = new Harmony("com.dcsobral.InventoryBeforeHotbar");
+        harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+        Debug.Log("Mod inventory-before-hotbar has been loaded!");
+    }
+
+    public void OnModUnload()
+    {
+        Debug.Log("Mod inventory-before-hotbar has been unloaded!");
+    }
+}
+
+[HarmonyPatch(typeof(Inventory), "FindSuitableSlot")]
+public class HarmonyPatch_InventoryBeforeHotbar
+{
+    [HarmonyPrefix]
+    static bool FindSuitableSlot(ref Slot __result, Inventory __instance, Item_Base stackableItem)
+    {
+        if (stackableItem == null)
+        {
+            return true;
+        } else
+        {
+            // Most of this is a copy of Inventory#FindSuitableSLot
+            // it needs to be kept up-to-date with Raft's
+            bool isStackable = stackableItem.settings_Inventory.Stackable;
+            Slot slot = null;
+            foreach (Slot slot2 in __instance.allSlots)
+            {
+                // Changed slot condition from slot == null
+                if (slot2.IsEmpty && slot2.active && (slot == null || slot.slotType == SlotType.Hotbar))
+                {
+                    slot = slot2;
+                }
+
+                if (isStackable && slot2.active && !slot2.StackIsFull() && !slot2.IsEmpty && slot2.itemInstance.UniqueIndex == stackableItem.UniqueIndex)
+                {
+                    // Changed from return slot2 to conform to method patching
+                    __result = slot2;
+                    return false;
+                }
+            }
+            // Changed from return slot to conform to method patching
+            __result = slot;
+            return false;
+        }
+    }
+}
